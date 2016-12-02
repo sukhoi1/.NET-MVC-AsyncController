c38a9321aba0451a89d3ab7f0c41c161951ef1de
Anton Lygin <lygin@live.com>
2016-12-02 23:30:16 +0300

    v0.001 - AsyncController initial commit;

---
+++ AsyncController/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AsyncController
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            if (bool.Parse(ConfigurationManager.AppSettings["DefaultControllerIsDemo"]) == false)
+            {
+                routes.MapRoute(
+                    name: "NewRoute",
+                    url: "App/Do{action}",
+                    defaults: new { controller = "Home" }
+                    );
+            }
+            else
+            {
+                routes.MapRoute(
+                    name: "NewRoute",
+                    url: "App/Do{action}",
+                    defaults: new { controller = "Demo" }
+                    );
+            }
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AsyncController/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AsyncController.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
