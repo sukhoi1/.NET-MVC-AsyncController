c541c31e87c1a206d0543993d0328b7f26eecd20
Anton Lygin <lygin@live.com>
2016-12-04 23:57:19 +0300

    v0.003 - async/await example;

--- AsyncController/Controllers/AsyncController.cs
+++ AsyncController/Controllers/AsyncController.cs
@@ -1,12 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using AsyncController.Service;
 
 namespace AsyncController.Controllers
 {
-    public class AsyncController : Controller
+    public class AsyncHomeController : System.Web.Mvc.AsyncController
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string data = await Task<string>.Factory.StartNew(() =>
+            {
+                return new RemoteService().GetRemoteData();
+            });
+
+            return View((object)data);
+        }
+
+        public async Task<ActionResult> Index2()
+        {
+            string data = await new RemoteService().GetRemoteDataAsync();
+            return View((object)data);
         }
     }
 }
---
+++ AsyncController/Service/RemoteService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncController.Service
+{
+    public class RemoteService
+    {
+        public string GetRemoteData()
+        {
+            Thread.Sleep(2000);
+            return "Hello from the other world";
+        }
+
+        public async Task<string> GetRemoteDataAsync()
+        {
+            return await Task<string>.Factory.StartNew(() => {
+                Thread.Sleep(2000);
+                return "Hello from the other world";
+            });
+        }
+    }
+}
